5c93ea63203b801722f3fc1b17bb23dc99132f09
bklanuradha <92729@fhss.sjp.ac.lk>
2022-02-16 14:21:44 +0000

    Finished-Quiz-App

--- src/Components/EndScreen.js
+++ src/Components/EndScreen.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { QuizContext } from "../Helpers/Contexts";
+import {Questions} from '../Helpers/QuestionBank';
+import "../App.css";
 
 function EndScreen() {
-  return (
-    <div>EndScreen</div>
-  )
+  const { score, setScore, setGameState } = useContext(QuizContext);
+
+  const restartQuiz = () => {
+    setScore(0);
+    setGameState("menu");
+  }
+  return <div className="EndScreen">
+    <h1>Quiz Finished</h1>
+    <h3>{score} / {Questions.length} </h3>
+    <button onClick={restartQuiz}>Restart Quiz</button>
+  </div>;
 }
 
-export default EndScreen
+export default EndScreen;
