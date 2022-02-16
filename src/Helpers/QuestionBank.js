b4fd026abc6b13f5c846fc321523199594195318
bklanuradha <92729@fhss.sjp.ac.lk>
2022-02-16 13:54:53 +0000

    Insert-Quiz

---
+++ src/Helpers/QuestionBank.js
@@ -0,0 +1,28 @@
+export const Questions = [
+  {
+    prompt: "What is 1+3?",
+    OptionA: "24",
+    OptionB: "21",
+    OptionC: "4",
+    OptionD: "2",
+    answer: "C",
+  },
+
+  {
+    prompt: "Which is a Streaming Platform?",
+    OptionA: "TWitter",
+    OptionB: "Twitch",
+    OptionC: "Google",
+    OptionD: "Robinhood",
+    answer: "B",
+  },
+
+  {
+    prompt: "Best tech youtube channel?",
+    OptionA: "PedroTech",
+    OptionB: "Dev Ed",
+    OptionC: "Fireship",
+    OptionD: "TechWithTim",
+    answer: "A",
+  },
+];
